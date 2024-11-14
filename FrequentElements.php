49a721bab7278106add0c5e5c277b7752dc307fa
IzotovAA <izotovandreiy@gmail.com>
2024-11-14 09:36:56 +0300

    add frequent elements

---
+++ FrequentElements.php
@@ -0,0 +1,77 @@
+<?php
+
+class Solution1 {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer[]
+     */
+    public function topKFrequent(array $nums, int $k): array
+    {
+        $temp = [];
+        $result = [];
+
+        foreach ($nums as $num) {
+            if (array_key_exists($num, $temp)) {
+                $temp[$num]++;
+            } else {
+                $temp[$num] = 1;
+            }
+        }
+
+        uasort($temp, [$this, 'compare']);
+
+        foreach ($temp as $num => $count) {
+            if ($k > 0) {
+                $result[] = $num;
+            } else {
+                break;
+            }
+            $k--;
+        }
+
+        return $result;
+    }
+
+    private function compare($a, $b): int
+    {
+        if ($a == $b) {
+            return 0;
+        }
+
+        return ($a < $b) ? 1 : -1;
+    }
+}
+
+class Solution2 {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer[]
+     */
+    public function topKFrequent(array $nums, int $k): array
+    {
+        $temp = [];
+        $result = [];
+
+        foreach ($nums as $num) {
+            if (array_key_exists($num, $temp)) {
+                $temp[$num]++;
+            } else {
+                $temp[$num] = 1;
+            }
+        }
+
+        while ($k > 0) {
+            $maxCount = max($temp);
+            $maxValue = array_search($maxCount, $temp);
+            $result[] = $maxValue;
+            $temp[$maxValue] = 0;
+            $k--;
+        }
+
+        return $result;
+    }
+}
